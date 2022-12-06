98367e76c402c57546f1b56d7e20427799dcc37b
Ivan Fefer <ivan.fefer@mongodb.com>
2022-12-06 09:53:47 +0100

    Day 6

---
+++ d6/pipeline_p1.js
@@ -0,0 +1,49 @@
+[
+  {
+    '$project': {
+      '_id': 0, 
+      'substring': {
+        '$map': {
+          'input': {'$range': [0, {'$subtract': [{'$strLenBytes': '$signal'}, 3]}], 
+          'as': 'index', 
+          'in': {
+            '$map': {
+              'input': {'$range': ['$$index', {'$add': ['$$index', 4]}]}, 
+              'as': 'subindex', 
+              'in': {'$substrBytes': ['$signal', '$$subindex', 1]}
+            }
+          }
+        }
+      }
+    }
+  }, {
+    '$unwind': {
+      'path': '$substring', 
+      'includeArrayIndex': 'index'
+    }
+  }, {
+    '$unwind': {
+      'path': '$substring', 
+      'includeArrayIndex': 'subindex'
+    }
+  }, {
+    '$group': {
+      '_id': '$index', 
+      'uniqueCharacters': {
+        '$addToSet': '$substring'
+      }
+    }
+  }, {
+    '$match': {
+      '$expr': {
+        '$eq': [{'$size':'$uniqueCharacters'}, 4]
+      }
+    }
+  }, {
+    '$sort': {'_id': 1}
+  }, {
+    '$limit': 1
+  }, {
+    '$project': {'_id': {'$add': ['$_id', 4]}}
+  }
+]
