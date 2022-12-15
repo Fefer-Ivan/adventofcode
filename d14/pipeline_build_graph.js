736f9f8627f61a61fd33007c80f1e3259d4d0f45
Ivan Fefer <ivan.fefer@mongodb.com>
2022-12-15 20:06:01 +0100

    Better graph

--- d14/pipeline_build_graph.js
+++ d14/pipeline_build_graph.js
@@ -3,35 +3,36 @@
     '$addFields': {
       'allPoints': {
         '$reduce': {
-          'input': { '$range': [ 0, { '$subtract': [ { '$size': '$points' }, 1 ] } ] }, 
-          'initialValue': [], 
+          'input': {
+            '$range': [ 0, { '$subtract': [ { '$size': '$points' }, 1 ] } ]
+          },
+          'initialValue': [],
           'in': {
             '$concatArrays': [
               '$$value', {
                 '$let': {
                   'vars': {
-                    'from': { '$arrayElemAt': [ '$points', '$$this' ] }, 
+                    'from': { '$arrayElemAt': [ '$points', '$$this' ] },
                     'to': { '$arrayElemAt': [ '$points', { '$add': [ '$$this', 1 ] } ] }
-                  }, 
+                  },
                   'in': {
                     '$let': {
                       'vars': {
-                        'sx': { '$min': [ '$$from.x', '$$to.x' ] }, 
-                        'fx': { '$add': [ { '$max': [ '$$from.x', '$$to.x' ] }, 1 ] }, 
-                        'sy': { '$min': [ '$$from.y', '$$to.y' ] }, 
+                        'sx': { '$min': [ '$$from.x', '$$to.x' ] },
+                        'fx': { '$add': [ { '$max': [ '$$from.x', '$$to.x' ] }, 1 ] },
+                        'sy': { '$min': [ '$$from.y', '$$to.y' ] },
                         'fy': { '$add': [ { '$max': [ '$$from.y', '$$to.y' ] }, 1 ] }
-                      }, 
+                      },
                       'in': {
                         '$reduce': {
-                          'input': { '$range': [ '$$sx', '$$fx' ] }, 
-                          'initialValue': [], 
+                          'input': { '$range': [ '$$sx', '$$fx' ] },
+                          'initialValue': [],
                           'in': {
                             '$concatArrays': [
-                              '$$value',
-                              {
+                              '$$value', {
                                 '$map': {
-                                  'input': { '$range': [ '$$sy', '$$fy' ] }, 
-                                  'as': 'y', 
+                                  'input': { '$range': [ '$$sy', '$$fy' ] },
+                                  'as': 'y',
                                   'in': { 'x': '$$this', 'y': '$$y' }
                                 }
                               }
@@ -52,87 +53,50 @@
     '$unwind': { 'path': '$allPoints' }
   }, {
     '$group': {
-      '_id': null, 
-      'occupied': { '$addToSet': '$allPoints' }, 
-      'maxY': {
-        '$max': {
-          '$add': [
-            '$allPoints.y', 1
-          ]
-        }
-      }
+      '_id': null,
+      'occupied': { '$addToSet': '$allPoints' },
+      'maxY': { '$max': { '$add': [ '$allPoints.y', 1 ] } }
     }
   }, {
     '$addFields': {
       'graph': {
         '$map': {
-          'input': {
-            '$range': [
-              250, 750
-            ]
-          }, 
-          'as': 'x', 
+          'input': { '$range': [ 0, { '$add': [ '$maxY', 1 ] } ] },
+          'as': 'y',
           'in': {
             '$map': {
-              'input': {
-                '$range': [
-                  0, {
-                    '$add': [
-                      '$maxY', 1
-                    ]
-                  }
-                ]
-              }, 
-              'as': 'y', 
+              'input': {'$range': [{'$subtract': [500, '$$y']}, {'$add': [500, '$$y', 1]}]},
+              'as': 'x',
               'in': {
                 '$let': {
                   'vars': {
-                    'point': {
-                      'x': '$$x', 
-                      'y': '$$y'
-                    }
-                  }, 
+                    'point': { 'x': '$$x', 'y': '$$y' }
+                  },
                   'in': {
                     '$cond': [
+                      { '$in': [ '$$point', '$occupied' ] },
+                      null,
                       {
-                        '$in': [
-                          '$$point', '$occupied'
-                        ]
-                      }, null, {
-                        'point': '$$point', 
+                        'point': '$$point',
                         'deps': {
-                          '$cond': [
-                            {
-                              '$gt': [
-                                {
-                                  '$add': [
-                                    '$$y', 1
-                                  ]
-                                }, '$maxY'
-                              ]
-                            }, [], {
-                              '$map': {
-                                'input': {
-                                  '$range': [
-                                    -1, 2
-                                  ]
-                                }, 
-                                'as': 'd', 
-                                'in': {
-                                  'x': {
-                                    '$add': [
-                                      '$$x', '$$d'
-                                    ]
-                                  }, 
-                                  'y': {
-                                    '$add': [
-                                      '$$y', 1
-                                    ]
-                                  }
-                                }
-                              }
+                          '$map': {
+                            'input': { '$range': [ -1, 2 ] },
+                            'as': 'd',
+                            'in': {
+                              'x': { '$add': [ '$$x', '$$d' ] },
+                              'y': { '$add': [ '$$y', 1 ] }
                             }
-                          ]
+                          }
+                        },
+                        'reverse_deps': {
+                          '$map': {
+                            'input': { '$range': [ -1, 2 ] },
+                            'as': 'd',
+                            'in': {
+                              'x': { '$add': [ '$$x', '$$d' ] },
+                              'y': { '$subtract': [ '$$y', 1 ] }
+                            }
+                          }
                         }
                       }
                     ]
@@ -146,11 +110,11 @@
     }
   }, {
     '$project': {
-      '_id': 0, 
+      '_id': 0,
       'graph': {
         '$reduce': {
-          'input': '$graph', 
-          'initialValue': [], 
+          'input': '$graph',
+          'initialValue': [],
           'in': { '$concatArrays': [ '$$value', '$$this' ] }
         }
       }
