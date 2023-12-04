2955a8f22f904526a769d29472465985ae96652e
Ivan Fefer <ivan.fefer@mongodb.com>
2023-12-04 12:23:55 +0100

    2023 day 4

---
+++ 2023/d04/pipeline_1.js
@@ -0,0 +1,54 @@
+[
+  {
+    '$project': { 'line': { '$split': [ '$data', '\n' ] } }
+  }, {
+    '$unwind': { 'path': '$line', 'includeArrayIndex': 'index' }
+  }, {
+    '$project': {
+      'index': 1, 
+      'card': {
+        '$map': {
+          'input': { '$split': [ { '$arrayElemAt': [ { '$split': [ '$line', ':' ] }, 1 ] }, '|' ] }, 
+          'as': 'numbers', 
+          'in': {
+            '$map': {
+              'input': { '$regexFindAll': { 'input': '$$numbers', 'regex': new RegExp('\d+') } }, 
+              'as': 'matches', 
+              'in': { '$toInt': '$$matches.match'
+              }
+            }
+          }
+        }
+      }
+    }
+  }, {
+    '$addFields': {
+      'score': {
+        '$let': {
+          'vars': {
+            'size': { '$size': { '$setIntersection': [
+                  { '$arrayElemAt': [ '$card', 0 ] }, {
+                    '$arrayElemAt': [ '$card', 1 ] }
+                ]
+              }
+            }
+          }, 
+          'in': {
+            '$cond': {
+              'if': { '$eq': [ '$$size', 0 ] }, 
+              'then': 0, 
+              'else': { '$pow': [ 2, { '$subtract': [ '$$size', 1 ] } ] }
+            }
+          }
+        }
+      }
+    }
+  }, {
+    '$group': {
+      '_id': '$_id', 
+      'totalScore': {
+        '$sum': '$score'
+      }
+    }
+  }
+]
---
+++ 2023/d04/pipeline_2.js
@@ -0,0 +1,79 @@
+[
+  {
+    '$project': { 'line': { '$split': [ '$data', '\n' ] } }
+  }, {
+    '$unwind': { 'path': '$line', 'includeArrayIndex': 'index' }
+  }, {
+    '$project': {
+      'index': 1, 
+      'card': {
+        '$map': {
+          'input': { '$split': [ { '$arrayElemAt': [ { '$split': [ '$line', ':' ] }, 1 ] }, '|' ] }, 
+          'as': 'numbers', 
+          'in': {
+            '$map': {
+              'input': { '$regexFindAll': { 'input': '$$numbers', 'regex': new RegExp('\d+') } }, 
+              'as': 'matches', 
+              'in': { '$toInt': '$$matches.match'
+              }
+            }
+          }
+        }
+      }
+    }
+  }, {
+    '$addFields': {
+      'score': {
+        '$size': {
+          '$setIntersection': [
+            { '$arrayElemAt': [ '$card', 0 ] }, {
+              '$arrayElemAt': [ '$card', 1 ] }
+          ]
+        }
+      }
+    }
+  }, {
+    '$group': { '_id': '$_id', 'cards': { '$push': '$$ROOT' } }
+  }, {
+    '$addFields': { 'cards': { '$sortArray': { 'input': '$cards', 'sortBy': { 'index': 1 } } } }
+  }, {
+    '$addFields': {
+      'cardCount': {
+        '$sum': {
+          '$reduce': {
+            'input': '$cards', 
+            'initialValue': {
+              '$map': {
+                'input': { '$range': [ 0, { '$size': '$cards' } ] }, 
+                'as': 'i', 
+                'in': 1
+              }
+            }, 
+            'in': {
+              '$map': {
+                'input': { '$range': [ 0, { '$size': '$cards' } ] }, 
+                'as': 'i', 
+                'in': {
+                  '$cond': {
+                    'if': { '$and': [
+                        { '$gt': [ '$$i', '$$this.index' ] }, {
+                          '$lte': [ '$$i', { '$add': [ '$$this.index', '$$this.score' ] } ] }
+                      ]
+                    }, 
+                    'then': {
+                      '$add': [
+                        { '$arrayElemAt': [ '$$value', '$$i' ] }, {
+                          '$arrayElemAt': [ '$$value', '$$this.index' ] }
+                      ]
+                    }, 
+                    'else': { '$arrayElemAt': [ '$$value', '$$i' ] }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+]
