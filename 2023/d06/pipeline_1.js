ee99ba6905351b07a38b525eddfbbc2efdd86b9c
Ivan Fefer <ivan.fefer@mongodb.com>
2023-12-06 11:29:55 +0100

    2023 day 6

---
+++ 2023/d06/pipeline_1.js
@@ -0,0 +1,104 @@
+[
+  {
+    '$project': {
+      'lines': {
+        '$map': {
+          'input': { '$split': [ '$data', '\n' ] }, 
+          'as': 'line', 
+          'in': {
+            '$map': {
+              'input': { '$regexFindAll': { 'input': '$$line', 'regex': new RegExp('\d+') } }, 
+              'as': 'match', 
+              'in': { '$toInt': '$$match.match' }
+            }
+          }
+        }
+      }
+    }
+  }, {
+    '$project': {
+      'races': {
+        '$zip': {
+          'inputs': [ { '$arrayElemAt': [ '$lines', 0 ] }, { '$arrayElemAt': [ '$lines', 1 ] } ]
+        }
+      }
+    }
+  }, {
+    '$addFields': {
+      'bounds': {
+        '$map': {
+          'input': '$races', 
+          'as': 'race', 
+          'in': {
+            '$let': {
+              'vars': {
+                't': { '$arrayElemAt': [ '$$race', 0 ] }, 
+                'd': { '$arrayElemAt': [ '$$race', 1 ] }
+              }, 
+              'in': {
+                '$let': {
+                  'vars': {
+                    'delta': {
+                      '$subtract': [ { '$pow': [ '$$t', 2 ] }, { '$multiply': [ 4, '$$d' ] } ]
+                    }
+                  }, 
+                  'in': {
+                    '$cond': {
+                      'if': { '$lt': [ '$$delta', 0 ] }, 
+                      'then': [ 0, 0 ], 
+                      'else': [
+                        { '$ceil': { '$add': [
+                              {
+                                '$divide': [ { '$subtract': [ { '$multiply': [ { '$sqrt': '$$delta' }, -1 ] }, '$$t' ] }, -2 ]
+                              }, -1
+                            ]
+                          }
+                        }, {
+                          '$floor': {
+                            '$add': [
+                              { '$divide': [ { '$subtract': [ { '$sqrt': '$$delta' }, '$$t' ] }, -2 ]
+                              }, 1
+                            ]
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }, {
+    '$addFields': {
+      'ways': {
+        '$reduce': {
+          'input': {
+            '$map': {
+              'input': '$bounds', 
+              'as': 'b', 
+              'in': {
+                '$add': [
+                  1, {
+                    '$subtract': [
+                      { '$arrayElemAt': [ '$$b', 0 ] }, {
+                        '$arrayElemAt': [ '$$b', 1 ] }
+                    ]
+                  }
+                ]
+              }
+            }
+          }, 
+          'initialValue': 1, 
+          'in': {
+            '$multiply': [
+              '$$value', '$$this'
+            ]
+          }
+        }
+      }
+    }
+  }
+]
---
+++ 2023/d06/pipeline_2.js
@@ -0,0 +1,76 @@
+[
+  {
+    '$project': {
+      'race': {
+        '$map': {
+          'input': { '$split': [ '$data', '\n' ] }, 
+          'as': 'line', 
+          'in': {
+            '$toLong': {
+              '$reduce': {
+                'input': { '$regexFindAll': { 'input': '$$line', 'regex': new RegExp('\d+') } }, 
+                'initialValue': '', 
+                'in': { '$concat': [ '$$value', '$$this.match' ]
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }, {
+    '$addFields': {
+      'bounds': {
+        '$let': {
+          'vars': {
+            't': { '$arrayElemAt': [ '$race', 0 ] }, 
+            'd': { '$arrayElemAt': [ '$race', 1 ] }
+          }, 
+          'in': {
+            '$let': {
+              'vars': {
+                'delta': { '$subtract': [ { '$pow': [ '$$t', 2 ] }, { '$multiply': [ 4, '$$d' ] } ] }
+              }, 
+              'in': {
+                '$cond': {
+                  'if': { '$lt': [ '$$delta', 0 ] }, 
+                  'then': [ 0, 0 ], 
+                  'else': [
+                    {
+                      '$ceil': {
+                        '$add': [
+                          { '$divide': [ { '$subtract': [ { '$multiply': [ { '$sqrt': '$$delta' }, -1 ] }, '$$t' ] }, -2 ]
+                          }, -1
+                        ]
+                      }
+                    }, {
+                      '$floor': {
+                        '$add': [
+                          { '$divide': [ { '$subtract': [ { '$sqrt': '$$delta' }, '$$t' ] }, -2 ]
+                          }, 1
+                        ]
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }, {
+    '$addFields': {
+      'ways': {
+        '$add': [
+          1, {
+            '$subtract': [
+              { '$arrayElemAt': [ '$bounds', 0 ] }, {
+                '$arrayElemAt': [ '$bounds', 1 ] }
+            ]
+          }
+        ]
+      }
+    }
+  }
+]
