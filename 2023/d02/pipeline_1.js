2d69ecc0e4cfbdb60a73c05b2f79c3211b231505
Ivan Fefer <ivan.fefer@mongodb.com>
2024-03-11 14:29:31 +0100

    compact and use first/last in day 2

--- 2023/d02/pipeline_1.js
+++ 2023/d02/pipeline_1.js
@@ -5,57 +5,35 @@ const pipeline1 = [
         input: { $split: ["$data", "\n"] },
         as: "line",
         in: {$let: {
-          vars: {
-            split: { $split: ["$$line", ":"] }
-          },
+          vars: { split: { $split: ["$$line", ":"] } },
           in: {
-            gameId: {$toInt: {$arrayElemAt: [
+            gameId: {$toInt: {$first:
               {$getField: {
                 input: { $regexFind: {
-                  input: { $arrayElemAt: ["$$split", 0 ] },
+                  input: { $first: "$$split" },
                   regex: /Game (\d+)/
                 }},
                 field: "captures"
               }},
-              0
-            ]}},
+            }},
             draws: {$map: {
-              input: { $split: [ { $arrayElemAt: ["$$split", 1 ] }, ";" ] },
+              input: { $split: [ { $last: "$$split" }, ";" ] },
               as: "draw",
               in: {$arrayToObject: {$map: {
                 input: { $regexFindAll: {input: "$$draw", regex: /(\d+) ([a-z]+)/ }},
                 as: "cube",
-                in: [
-                    { $arrayElemAt: [ "$$cube.captures", 1 ] },
-                    { $toInt: { $arrayElemAt: [ "$$cube.captures", 0 ] } },
-                ]
-              }}}
-            }}
-          }
-        }}
-      }}
-  }},
+                in: [ { $last: "$$cube.captures" },
+                    { $toInt: { $first: "$$cube.captures" } }]}}}}}}}}}}}},
   { $project: {
     sum: {$sum: {$map: {
       input: {$filter: {
         input: "$games",
         as: "game",
         cond: {$and: [
-          {$gte: [
-            12,
-            {$max: {$map: {input: "$$game.draws", in: "$$this.red"}}},
-          ]},
-          {$gte: [
-            13,
-            {$max: {$map: {input: "$$game.draws", in: "$$this.green"}}},
-          ]},
-          {$gte: [
-            14,
-            {$max: {$map: {input: "$$game.draws", in: "$$this.blue"}}},
-          ]},
+          {$gte: [ 12, {$max: {$map: {input: "$$game.draws", in: "$$this.red"}}}, ]},
+          {$gte: [ 13, {$max: {$map: {input: "$$game.draws", in: "$$this.green"}}}, ]},
+          {$gte: [ 14, {$max: {$map: {input: "$$game.draws", in: "$$this.blue"}}}, ]},
         ]}
       }},
-      in: "$$this.gameId"
-    }}}
-  }}
+      in: "$$this.gameId"}}}}}
 ]; 
